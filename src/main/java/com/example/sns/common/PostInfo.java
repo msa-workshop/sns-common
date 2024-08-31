e9864a406f0d62b700ec4b56be492ddaa1733314
jheo <papercut@gmail.com>
2024-09-01 02:37:02 +0900

    update DTO

--- src/main/java/com/example/sns/common/PostInfo.java
+++ src/main/java/com/example/sns/common/PostInfo.java
@@ -4,7 +4,7 @@ import java.time.ZonedDateTime;
 
 public class PostInfo {
 
-    private int feedId;
+    private int postId;
     private String imageId;
     private int uploaderId;
     private String uploaderName;
@@ -15,8 +15,17 @@ public class PostInfo {
     public PostInfo() {
     }
 
-    public int getFeedId() {
-        return feedId;
+    public PostInfo(int postId, String imageId, int uploaderId, String uploaderName, ZonedDateTime uploadDatetime, String contents) {
+        this.postId = postId;
+        this.imageId = imageId;
+        this.uploaderId = uploaderId;
+        this.uploaderName = uploaderName;
+        this.uploadDatetime = uploadDatetime;
+        this.contents = contents;
+    }
+
+    public int getPostId() {
+        return postId;
     }
 
     public String getImageId() {
